29d50eed23514ea5a6d5c2b9167cad0c3f994107
bit <bit@DESKTOP-HPBJEHN>
2025-09-08 12:33:29 +0900

    board.sql

--- sql/board.sql
+++ sql/board.sql
@@ -3,3 +3,10 @@
 --
 
 desc board;
+
+ALTER TABLE board CHANGE order_no o_no INT(11) NOT NULL;
+
+ALTER TABLE board DROP COLUMN o_no;
+
+ALTER TABLE board 
+ADD COLUMN o_no INT(11) NOT NULL DEFAULT 0 AFTER g_no;
