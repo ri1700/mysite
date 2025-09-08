80085815895ad7dcf8c400681899059c721a99c9
bit <bit@DESKTOP-HPBJEHN>
2025-09-08 12:58:17 +0900

    mysite03 - aop#1

--- mysite03/src/main/java/com/bit2025/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/bit2025/mysite/repository/GuestbookRepository.java
@@ -2,14 +2,16 @@ package com.bit2025.mysite.repository;
 
 import java.util.List;
 import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
+
 import com.bit2025.mysite.vo.GuestbookVo;
 
 @Repository
 public class GuestbookRepository {
-	
 	@Autowired
 	private SqlSession sqlSession;
 	
